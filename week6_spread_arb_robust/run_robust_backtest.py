8c55c32fd009e4e39fef3b9155c1bcbf7e8872a1
Mathieu Chabirand <mathieu.chabirand@gmail.com>
2025-08-08 18:39:56 +0200

    CLI Parser implemented

--- week6_spread_arb_robust/run_robust_backtest.py
+++ week6_spread_arb_robust/run_robust_backtest.py
@@ -1,15 +1,34 @@
-from robust_spread_arb import PairBacktester
-import pandas as pd
 import json
 import os
+import logging
+import argparse
+from datetime import datetime
+from robust_spread_arb import PairBacktester
+import matplotlib as plt
 
 def run_backtest():
     script_dir = os.path.dirname(os.path.abspath(__file__))
     config_dir = os.path.join(script_dir, "config.json")
 
-    # Define backtest parameters __> put json file
+    # --- CLI Parser ---
+    parser = argparse.ArgumentParser(description="Run spread arbitrage backtest")
+    parser.add_argument("--config", type=str, default=config_dir, help="Path to config.json")
+    parser.add_argument("--window", type=float, help="Rolling window for z-score")
+    parser.add_argument("--z_entry", type=float, help="Z_score entry threshold")
+    parser.add_argument("--z_exit", type=float, help="Z_score exit threshold")
+    parser.add_argument("--capital", type=float, help="Capital in USD")
+    parser.add_argument("--plots", type=float, default=True, help="Display plots")
+    args = parser.parse_args()
+
+    # --- Load base config ---
     with open(config_dir, "r") as params_file:
         params = json.load(params_file)
+
+    # --- Override config.json with CLI arguments if provided ---
+    for keys in ["window", "z_entry", "z_exit", "capital", "plots"]:
+        val = getattr(args, keys)
+        if val is not None:
+            params[keys] = val
 
     # Instantiate and run backtest
     bt = PairBacktester(**params)
